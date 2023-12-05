12228ed882600b1d4327dad1dc242039b52e3ab0
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-12-05 17:14:47 +0000

    Add exercises

---
+++ FrogRace/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace FrogRace
+{
+    class Program
+    {
+        private static Queue<int> finishOrder;
+        private static object threadLock;
+
+        static void Main()
+        {
+            finishOrder = new Queue<int>();
+            
+            HashSet<Thread> threadSet = new HashSet<Thread>();
+
+            Thread thread1 = new Thread(FrogJump);
+            threadSet.Add(thread1);
+            Thread thread2 = new Thread(FrogJump);
+            threadSet.Add(thread2);
+            Thread thread3 = new Thread(FrogJump);
+            threadSet.Add(thread3);
+
+            thread1.Name = "T_One";
+            thread2.Name = "T_Two";
+            thread3.Name = "T_Three";
+
+            threadLock = new object();
+
+            thread1.Start(1);
+            thread2.Start(2);
+            thread3.Start(3);
+
+            foreach(Thread thread in threadSet)
+            {
+                thread.Join();
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("The race has ended!");
+            Console.WriteLine("First Place is Frog #" + finishOrder.Dequeue());
+            Console.WriteLine("Second Place is Frog #" + finishOrder.Dequeue());
+            Console.WriteLine("Third Place is Frog #" + finishOrder.Dequeue());
+            
+        }
+
+        private static void FrogJump(object frogNumber)
+        {
+            Random random = new Random((int)frogNumber);
+
+            for(int x = 0; x < 10; x++)
+            {
+                Thread.Sleep(random.Next(0,1));
+                Console.WriteLine("Frog " + frogNumber + " has completed jump #" + x + "!");
+            }
+            Console.WriteLine("Frog " + frogNumber + " has finished jumping!");
+
+            lock(threadLock)
+            {
+                finishOrder.Enqueue((int)frogNumber);
+            }
+        }
+    }
+}
---
+++ GoofyThreads/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace GoofyThreads
+{
+    class Program
+    {
+        private static List<int> numbers;
+
+        static void Main()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            numbers = new List<int> {1,2,3,4};
+
+            DateTime dt = DateTime.Now;
+            
+            Thread thread1 = new Thread(MultiplyBy5);
+            Thread thread2 = new Thread(MultiplyBy5);
+            Thread thread3 = new Thread(MultiplyBy5);
+
+            thread1.Start(1);
+            thread2.Start(2);
+            thread3.Start(3);
+
+            MultiplyBy5(0);
+
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+
+            Console.WriteLine("Time spent on multithread for operation: " + (DateTime.Now - dt).ToString());
+            Console.WriteLine("List: {" + numbers[0] + ","
+                                        + numbers[1] + ","
+                                        + numbers[2] + ","+
+                                          numbers[3] + "}"); 
+            
+            Console.WriteLine("");
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            numbers = new List<int> {1,2,3,4};
+
+            dt = DateTime.Now;
+
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                MultiplyBy5(i);
+            }
+
+            Console.WriteLine("Time spent on single thread for operation (using method): " + (DateTime.Now - dt).ToString());
+            Console.WriteLine("List: {" + numbers[0] + ","
+                                        + numbers[1] + ","
+                                        + numbers[2] + ","+
+                                          numbers[3] + "}");
+
+            Console.WriteLine("");
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            numbers = new List<int> {1,2,3,4};
+
+            dt = DateTime.Now;
+
+            MultiplyBy5(0);
+            MultiplyBy5(1);
+            MultiplyBy5(2);
+            MultiplyBy5(3);
+
+            Console.WriteLine("Time spent on single thread manual operation (using method): " + (DateTime.Now - dt).ToString());
+            Console.WriteLine("List: {" + numbers[0] + ","
+                                        + numbers[1] + ","
+                                        + numbers[2] + ","+
+                                          numbers[3] + "}"); 
+
+            Console.WriteLine("");
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            numbers = new List<int> {1,2,3,4};
+
+            dt = DateTime.Now;
+
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                numbers[i] *= 5;
+            }
+
+            Console.WriteLine("Time spent on single thread for operation (without method): " + (DateTime.Now - dt).ToString());
+            Console.WriteLine("List: {" + numbers[0] + ","
+                                        + numbers[1] + ","
+                                        + numbers[2] + ","+
+                                          numbers[3] + "}");
+
+            Console.WriteLine("");
+            
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            numbers = new List<int> {1,2,3,4};
+
+            dt = DateTime.Now;
+
+            numbers[0] *= 5;
+            numbers[1] *= 5;
+            numbers[2] *= 5;
+            numbers[3] *= 5;
+
+            Console.WriteLine("Time spent on single thread manual operation (without method): " + (DateTime.Now - dt).ToString());
+            Console.WriteLine("List: {" + numbers[0] + ","
+                                        + numbers[1] + ","
+                                        + numbers[2] + ","+
+                                          numbers[3] + "}");       
+        }
+
+        private static void MultiplyBy5(object index)
+        {
+            numbers[(int)index] *= 5;  
+        }
+    }
+}
